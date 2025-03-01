2e86dd66aad3e363c617fde7f6b3c65faa972a5b
tilahun goitom <tilahun1goitomg@gmail.com>
2025-03-01 05:08:35 -0800

    first commit

---
+++ data/index.ts
@@ -0,0 +1,356 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  // { name: "", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "I always know my work is shape|engineer thoughts to quality codes with nice user exprience ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/tilahun.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I working to catch with new upates on",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2 ",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "/new1.jpg",
+    spareImg: "",
+  },
+  // {
+  //   id: 4,
+  //   title: "Tech enthusiast with a passion for development.",
+  //   description: "",
+  //   className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+  //   imgClassName: "",
+  //   titleClassName: "justify-start",
+  //   img: "/grid.svg",
+  //   spareImg: "/b4.svg",
+  // },
+
+  //   {
+  //     id: 5,
+  //     title: "Currently building a JS Animation library",
+  //     description: "The Inside Scoop",
+  //     className: "md:col-span-3 md:row-span-2",
+  //     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+  //     titleClassName: "justify-center md:justify-start lg:justify-center",
+  //     img: "/b5.svg",
+  //     spareImg: "/grid.svg",
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "Do you want to start a project together?",
+  //     description: "",
+  //     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+  //     imgClassName: "",
+  //     titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+  //     img: "",
+  //     spareImg: "",
+  //   },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Free and peers courses",
+    des: "the project is about to provide free and peers courses for students will fasilate the learning process.",
+    img: "/addcontent.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.tilahun.com",
+  },
+  {
+    id: 2,
+    title: "authontications for user confidenciality",
+    des: "This emphasizes the use of authentication libraries to secure user data and prevent unauthorized access.",
+    img: "/clerks' auth.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Free and peers courses",
+    des: "the project is about to provide free and peers courses for students will fasilate the learning process.",
+    img: "/mycourses.png",
+    iconLists: [
+      "/java.svg",
+      "/python.svg",
+      "/javascript.svg",
+      "/re.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/three.svg",
+      "/c.svg",
+    ],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "online notes takers",
+    des: "this is supposed to enable user take events without worrying about the time and place.",
+    img: "/loginnote.png",
+    iconLists: [
+      "/java.svg",
+      "/python.svg",
+      "/javascript.svg",
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/three.svg",
+      "/gsap.svg",
+    ],
+    link: "/ui.tilahun.com",
+  },
+  {
+    id: 4,
+    title: "Shul shire referal",
+    des: "This project intended to automate the referal process of shul shire hospital company.",
+    img: "/shire referal.png",
+    iconLists: [
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/three.svg",
+      "/gsap.svg",
+      "/java.svg",
+      "/python.svg",
+      "/javascript.svg",
+    ],
+    link: "/ui.apple.com",
+  },
+  {
+    id: 4,
+    title: "Free and peers courses",
+    des: "the project is about to provide free and peers courses for students will fasilate the learning process.",
+    img: "/peerCourses1.png",
+    iconLists: [
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/three.svg",
+      "/gsap.svg",
+      "/shimering.png",
+      "/shimering.png",
+      "/shimering.png",
+    ],
+    link: "/ui.apple.com",
+  },
+  {
+    id: 4,
+    title: "Shul shire referal",
+    des: "This project intended to automate the referal process of shul shire hospital company.",
+    img: "/shiresingup.png",
+    iconLists: [
+      "/python.svg",
+      "/mongodb.svg",
+      "/javascript.svg",
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/three.svg",
+      "/gsap.svg",
+    ],
+    link: "/ui.apple.com",
+  },
+  {
+    id: 4,
+    title: "open market for everyone for free",
+    des: "This emphasizes any one with this app can explore arbitrary product from any one and can make deal ",
+    img: "/find product.png",
+    iconLists: [
+      "/python.svg",
+      "/mongodb.svg",
+      "/javascript.svg",
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/three.svg",
+      "/gsap.svg",
+    ],
+    link: "/ui.apple.com",
+  },
+  {
+    id: 4,
+    title: "react native app",
+    des: "this is a react native app for users to get their products and services at their finger tips.",
+    img: "/homephone.jpg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+  {
+    id: 4,
+    title: "react native app",
+    des: "this is a react native app for users to get their products and services at their finger tips.",
+    img: "/loginphone.jpg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+  {
+    id: 4,
+    title: "react native app",
+    des: "this is a react native app for users to get their products and services at their finger tips.",
+    img: "/homephone1.jpg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Professional software engineers are the architects of success. Their ability to navigate requirements with precision and creativity sets the stage for projects that exceed expectations and deliver lasting impact.",
+    name: "",
+    title: "",
+  },
+  {
+    quote:
+      "In the hands of a dedicated software engineer, requirements are not just checkboxes; they are the foundation for success. Their expertise and attention to detail turn visions into reality, ensuring projects thrive.",
+    name: "",
+    title: "",
+  },
+  {
+    quote:
+      "When a skilled software engineer takes on a project, you can be confident in its success. Their unwavering focus on requirements and commitment to quality transforms challenges into opportunities.",
+    name: "software experts",
+    title: "",
+  },
+  {
+    quote:
+      "A true professional software engineer not only meets requirements but transforms them into innovative solutions. Their commitment to excellence ensures that every project, once embarked upon, is destined for success.",
+    name: "software experts",
+    title: "",
+  },
+  {
+    quote:
+      "The hallmark of a professional software engineer is their alignment with project requirements. Their meticulous approach and passion for problem-solving pave the way for successful outcomes every time.",
+    name: "software experts",
+    title: "",
+  },
+];
+
+export const companies = [
+  {
+    id: 2,
+    name: "E-commerce apps",
+    img: "/paypal.svg",
+    nameImg: "",
+  },
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "",
+  },
+  {
+    id: 2,
+    name: "favorite IDE",
+    img: "/vscode.svg",
+    nameImg: "",
+  },
+  {
+    id: 3,
+    name: "VCS",
+    img: "/bash.svg",
+    nameImg: "",
+  },
+  {
+    id: 4,
+    name: "TypeScript",
+    img: "/ts.svg",
+    nameImg: "",
+  },
+  {
+    id: 4,
+    name: "For AI & ML",
+    img: "/python.svg",
+    nameImg: "",
+  },
+  {
+    id: 4,
+    name: "packages",
+    img: "/npm.svg",
+    nameImg: "",
+  },
+  {
+    id: 4,
+    name: "JS",
+    img: "/javascript.svg",
+    nameImg: "",
+  },
+  //   //{
+  //   //   id: 5,
+  //   //   name: "docker.",
+  //   //   img: "/dock.svg",
+  //   //   nameImg: "/dockerName.svg",
+  //   // },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "a freelancer full stack developer-TG dev Tech",
+    desc: "As a passionate full-stack developer at TG Dev Tech, I leveraged my expertise in Java, Next.js, JavaScript, and C++ to design and develop robust, scalable web applications. I contributed to both front-end and back-end development, ensuring seamless integration and optimal performance across the stack. My experience spans building interactive user interfaces, optimizing server-side logic, and solving complex problems with efficient algorithms. This role has deepened my love for creating end-to-end solutions and reinforced my commitment to mastering cutting-edge technologies in software engineering.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev -TG dev Tech",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title:
+      "designer with photoshop, fugma, illustrator and I'm proficient in management tools like Excel, Word, and PowerPoint ...",
+    desc: "As a creative designer, I utilized tools like Photoshop, Figma, and Adobe Illustrator to bring innovative ideas to life. I led the development of a mobile app for a client, overseeing the entire process from initial concept design to final deployment on app stores. My role involved creating visually stunning interfaces, prototyping user experiences, and ensuring seamless collaboration between design and development teams. This experience has strengthened my ability to blend creativity with functionality, delivering designs that are both beautiful and user-friendly.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: " /adobe.svg",
+  },
+  // {
+  //   id: 4,
+  //   title: "Lead Frontend Developer",
+  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp4.svg",
+  // },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    info: "https://github.com/kua-University/Peer-courses-Tilahun",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    info: "https://twitter.com/tilahun_goitom",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    info: "https://www.linkedin.com/in/tilahun-goitom-559401302/",
+  },
+];

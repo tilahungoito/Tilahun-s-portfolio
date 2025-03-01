2c112c0426bec46b4c0236f1b002ea1c6e7bb65f
tilahun goitom <tilahun1goitomg@gmail.com>
2025-03-01 08:49:00 -0800

    Oops deployment error1 fixed

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,4 +1,5 @@
-import * as Sentry from '@sentry/nextjs';
+//import * as Sentry from '@sentry/nextjs';
+import Sentry from "@sentry/nextjs";
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {

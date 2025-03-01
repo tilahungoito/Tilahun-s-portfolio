68c2a475ca9c941af2f4292572f861c53c3bc636
tilahun goitom <tilahun1goitomg@gmail.com>
2025-03-01 09:33:10 -0800

    deployment erro3

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -19,8 +19,8 @@ const RecentProjects = () => {
             key={item.id}
           >
             <PinContainer
-              title="/ui.aceternity.com"
-              href="https://twitter.com/mannupaaji"
+              title="/ui.tilahun.goitom.com"
+              href="https://github.com/tilahungoito/Tilahun-s-portfolio"
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
